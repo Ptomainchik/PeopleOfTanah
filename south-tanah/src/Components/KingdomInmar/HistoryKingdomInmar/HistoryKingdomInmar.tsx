fe5aca49674fa6e9c549fe2852381911e648564e
Ptomainchik <fantasticdenis@mail.ru>
2025-06-18 21:45:57 +0300

    HistoriesImages

--- south-tanah/src/Components/KingdomInmar/HistoryKingdomInmar/HistoryKingdomInmar.tsx
+++ south-tanah/src/Components/KingdomInmar/HistoryKingdomInmar/HistoryKingdomInmar.tsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 import classes from "../../../Styles/History.module.css";
 import { BackButton } from "../../StartPage/Buttons/BackButton/BackButton";
 import { HomeButton } from "../../StartPage/Buttons/HomeButton/HomeButton";
-import Image from "../../../Images/InmarForestBackroundChoice.jpg";
 import Background from "../../../Images/InmarForest.jpg";
+import KingOfInmar from "../../../Images/KingOfInmar.jpg";
+import BattlteInTheRuins from "../../../Images/BattlteInTheRuins.jpg";
+import Mesa from "../../../Images/Mesa.jpg";
+import SecretOrder from "../../../Images/SecretOrder.jpg";
+import SoldiersOfInmar from "../../../Images/SoldiersOfInmar.jpg";
 
 export const HistoryKingdomInmar = () => {
     const [showModalId, setshowModalId] = useState(null);
@@ -34,54 +38,44 @@ export const HistoryKingdomInmar = () => {
         <div className={classes.historyPage} style={{ backgroundImage: `url(${Background})` }}>
             <div className={classes.topbar} style={{background: "darkred"}}><HomeButton/>История Королевства Инмар<BackButton/></div>
             <button className={classes.buttonOpenModal1} id={"1"} onClick={() => {handleOpenModal("1")}}><p className={classes.titleChapterHistory} style={{color: "darkred", WebkitTextStroke: "1px black"}}>Глава 1</p></button>
-            {showModalId === "1" && (<div className={buttonClassLeft} id={"1"}>
+            {showModalId === "1" && (<div className={buttonClassLeft} id={"1"} style={{border: "solid 0.3vw darkred"}}>
                 <h4 style={{color: "darkred"}}>Глава 1</h4>
                 <p style={{color: "darkred"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque, maiores hic dignissimos ad omnis. 
                 Mollitia sunt minima ad minus quidem voluptatem soluta cum. Ad velit eligendi assumenda reiciendis aliquam?
                 </p>
-                <div className={classes.overlayImageModalHistory} style={{background:"darkred"}}>
-                    <img className={classes.imageModalHistory} src={Image} alt="Image1" draggable={"false"}/> 
-                </div>
+                <img className={classes.imageModalHistory} src={KingOfInmar} alt="KingOfInmar" draggable={"false"} style={{borderRight: "solid 1vw darkred", borderBottom: "solid 1vw darkred"}}/>
                 <button className={classes.buttonCloseModal} id={"1"} onClick={handleCloseModal}>Закрыть</button></div>)}
             <button className={classes.buttonOpenModal2} id={"2"} onClick={() => {handleOpenModal("2")}}><p className={classes.titleChapterHistory} style={{color: "darkred", WebkitTextStroke: "1px black"}}>Глава 2</p></button>
-            {showModalId === "2" && (<div className={buttonClassRight} id={"2"}>
+            {showModalId === "2" && (<div className={buttonClassRight} id={"2"} style={{border: "solid 0.3vw darkred"}}>
                 <h4 style={{color: "darkred"}}>Глава 2</h4>
                 <p style={{color: "darkred"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque, maiores hic dignissimos ad omnis. 
                 Mollitia sunt minima ad minus quidem voluptatem soluta cum. Ad velit eligendi assumenda reiciendis aliquam?
                 </p>
-                <div className={classes.overlayImageModalHistory} style={{background:"darkred"}}>
-                    <img className={classes.imageModalHistory} src={Image} alt="Image2" draggable={"false"}/> 
-                </div>
+                <img className={classes.imageModalHistory} src={BattlteInTheRuins} alt="BattlteInTheRuins" draggable={"false"} style={{borderRight: "solid 1vw darkred", borderBottom: "solid 1vw darkred"}}/> 
                 <button className={classes.buttonCloseModal} id={"2"} onClick={handleCloseModal}>Закрыть</button></div>)}
             <button className={classes.buttonOpenModal3} id={"3"} onClick={() => {handleOpenModal("3")}}><p className={classes.titleChapterHistory} style={{color: "darkred", WebkitTextStroke: "1px black"}}>Глава 3</p></button>
-            {showModalId === "3" && (<div className={buttonClassLeft} id={"3"}>
+            {showModalId === "3" && (<div className={buttonClassLeft} id={"3"} style={{border: "solid 0.3vw darkred"}}>
                 <h4 style={{color: "darkred"}}>Глава 3</h4>
                 <p style={{color: "darkred"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque, maiores hic dignissimos ad omnis. 
                 Mollitia sunt minima ad minus quidem voluptatem soluta cum. Ad velit eligendi assumenda reiciendis aliquam?
                 </p>
-                <div className={classes.overlayImageModalHistory} style={{background:"darkred"}}>
-                    <img className={classes.imageModalHistory} src={Image} alt="Image3" draggable={"false"}/> 
-                </div>
+                <img className={classes.imageModalHistory} src={Mesa} alt="Mesa" draggable={"false"} style={{borderRight: "solid 1vw darkred", borderBottom: "solid 1vw darkred"}}/> 
                 <button className={classes.buttonCloseModal} id={"3"} onClick={handleCloseModal}>Закрыть</button></div>)}
             <button className={classes.buttonOpenModal4} id={"4"} onClick={() => {handleOpenModal("4")}}><p className={classes.titleChapterHistory} style={{color: "darkred", WebkitTextStroke: "1px black"}}>Глава 4</p></button>
-            {showModalId === "4" && (<div className={buttonClassRight} id={"4"}>
+            {showModalId === "4" && (<div className={buttonClassRight} id={"4"} style={{border: "solid 0.3vw darkred"}}>
                 <h4 style={{color: "darkred"}}>Глава 4</h4>
                 <p style={{color: "darkred"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque, maiores hic dignissimos ad omnis. 
                 Mollitia sunt minima ad minus quidem voluptatem soluta cum. Ad velit eligendi assumenda reiciendis aliquam?
                 </p>
-                <div className={classes.overlayImageModalHistory} style={{background:"darkred"}}>
-                    <img className={classes.imageModalHistory} src={Image} alt="Image4" draggable={"false"}/> 
-                </div>
+                <img className={classes.imageModalHistory} src={SecretOrder} alt="SecretOrder" draggable={"false"} style={{borderRight: "solid 1vw darkred", borderBottom: "solid 1vw darkred"}}/> 
                 <button className={classes.buttonCloseModal} id={"4"} onClick={handleCloseModal}>Закрыть</button></div>)}
             <button className={classes.buttonOpenModal5} id={"5"} onClick={() => {handleOpenModal("5")}}><p className={classes.titleChapterHistory} style={{color: "darkred", WebkitTextStroke: "1px black"}}>Глава 5</p></button>
-            {showModalId === "5" && (<div className={buttonClassLeft} id={"5"}>
+            {showModalId === "5" && (<div className={buttonClassLeft} id={"5"} style={{border: "solid 0.3vw darkred"}}>
                 <h4 style={{color: "darkred"}}>Глава 5</h4>
                 <p style={{color: "darkred"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque, maiores hic dignissimos ad omnis. 
                 Mollitia sunt minima ad minus quidem voluptatem soluta cum. Ad velit eligendi assumenda reiciendis aliquam?
                 </p>
-                <div className={classes.overlayImageModalHistory} style={{background:"darkred"}}>
-                    <img className={classes.imageModalHistory} src={Image} alt="Image5" draggable={"false"}/> 
-                </div>
+                <img className={classes.imageModalHistory} src={SoldiersOfInmar} alt="SoldiersOfInmar" draggable={"false"} style={{borderRight: "solid 1vw darkred", borderBottom: "solid 1vw darkred"}}/> 
                 <button className={classes.buttonCloseModal} id={"5"} onClick={handleCloseModal}>Закрыть</button></div>)}
         </div>
     )
--- south-tanah/src/Components/СonfederationOfTribes/HistoryConfederation/HistoryConfederation.tsx
+++ south-tanah/src/Components/СonfederationOfTribes/HistoryConfederation/HistoryConfederation.tsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 import classes from "../../../Styles/History.module.css";
 import { BackButton } from "../../StartPage/Buttons/BackButton/BackButton";
 import { HomeButton } from "../../StartPage/Buttons/HomeButton/HomeButton";
-import Image from "../../../Images/DesertConfederationBackgroundChoice.jpg";
 import Background from "../../../Images/DesertConfederation.jpg";
+import Eltabar from "../../../Images/Eltabar.jpg";
+import Insurrection from "../../../Images/Insurrection.jpg";
+import Temple from "../../../Images/Temple.jpg";
+import Caravans from "../../../Images/Caravans.jpg";
+import SoldiersOfRegistan from "../../../Images/SoldiersOfRegistan.jpg";
 
 export const HistoryConfederation = () => {
     const [showModalId, setshowModalId] = useState(null);
@@ -34,55 +38,45 @@ export const HistoryConfederation = () => {
         <div className={classes.historyPage} style={{ backgroundImage: `url(${Background})` }}>
             <div className={classes.topbar} style={{background: "goldenrod"}}><HomeButton/>История Конфедерации племён<BackButton/></div>
             <button className={classes.buttonOpenModal1} id={"1"} onClick={() => {handleOpenModal("1")}}><p className={classes.titleChapterHistory} style={{color: "goldenrod", WebkitTextStroke: "1px black"}}>Глава 1</p></button>
-            {showModalId === "1" && (<div className={buttonClassLeft} id={"1"}>
+            {showModalId === "1" && (<div className={buttonClassLeft} id={"1"} style={{border: "solid 0.3vw goldenrod"}}>
                 <h4 style={{color:"goldenrod"}}>Глава 1</h4> 
                 <p style={{color:"goldenrod"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque, maiores hic dignissimos ad omnis. 
                 Mollitia sunt minima ad minus quidem voluptatem soluta cum. Ad velit eligendi assumenda reiciendis aliquam?
                 </p>
-                <div className={classes.overlayImageModalHistory} style={{background:"goldenrod"}}>
-                    <img className={classes.imageModalHistory} src={Image} alt="Image1" draggable={"false"}/> 
-                </div> 
-                    <button className={classes.buttonCloseModal} id={"1"} onClick={handleCloseModal}>Закрыть</button>
+                <img className={classes.imageModalHistory} src={Eltabar} alt="Eltabar" draggable={"false"} style={{borderRight: "solid 1vw goldenrod", borderBottom: "solid 1vw goldenrod"}}/> 
+                <button className={classes.buttonCloseModal} id={"1"} onClick={handleCloseModal}>Закрыть</button>
             </div>)}
             <button className={classes.buttonOpenModal2} id={"2"} onClick={() => {handleOpenModal("2")}}><p className={classes.titleChapterHistory} style={{color: "goldenrod", WebkitTextStroke: "1px black"}}>Глава 2</p></button>
-            {showModalId === "2" && (<div className={buttonClassRight} id={"2"}>
+            {showModalId === "2" && (<div className={buttonClassRight} id={"2"} style={{border: "solid 0.3vw goldenrod"}}>
                 <h4 style={{color:"goldenrod"}}>Глава 2</h4>
                 <p style={{color:"goldenrod"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque, maiores hic dignissimos ad omnis. 
                 Mollitia sunt minima ad minus quidem voluptatem soluta cum. Ad velit eligendi assumenda reiciendis aliquam?
                 </p>
-                <div className={classes.overlayImageModalHistory} style={{background:"whegoldenrodat"}}>
-                    <img className={classes.imageModalHistory} src={Image} alt="Image2" draggable={"false"}/> 
-                </div>
+                <img className={classes.imageModalHistory} src={Insurrection} alt="Insurrection" draggable={"false"} style={{borderRight: "solid 1vw goldenrod", borderBottom: "solid 1vw goldenrod"}}/> 
                 <button className={classes.buttonCloseModal} id={"2"} onClick={handleCloseModal}>Закрыть</button></div>)}
             <button className={classes.buttonOpenModal3} id={"3"} onClick={() => {handleOpenModal("3")}}><p className={classes.titleChapterHistory} style={{color: "goldenrod", WebkitTextStroke: "1px black"}}>Глава 3</p></button>
-            {showModalId === "3" && (<div className={buttonClassLeft} id={"3"}>
+            {showModalId === "3" && (<div className={buttonClassLeft} id={"3"} style={{border: "solid 0.3vw goldenrod"}}>
                 <h4 style={{color:"goldenrod"}}>Глава 3</h4>
                 <p style={{color:"goldenrod"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque, maiores hic dignissimos ad omnis. 
                 Mollitia sunt minima ad minus quidem voluptatem soluta cum. Ad velit eligendi assumenda reiciendis aliquam?
                 </p>
-                <div className={classes.overlayImageModalHistory} style={{background:"goldenrod"}}>
-                    <img className={classes.imageModalHistory} src={Image} alt="Image3" draggable={"false"}/> 
-                </div>
+                <img className={classes.imageModalHistory} src={Temple} alt="Image3" draggable={"false"} style={{borderRight: "solid 1vw goldenrod", borderBottom: "solid 1vw goldenrod"}}/> 
                 <button className={classes.buttonCloseModal} id={"3"} onClick={handleCloseModal}>Закрыть</button></div>)}
             <button className={classes.buttonOpenModal4} id={"4"} onClick={() => {handleOpenModal("4")}}><p className={classes.titleChapterHistory} style={{color: "goldenrod", WebkitTextStroke: "1px black"}}>Глава 4</p></button>
-            {showModalId === "4" && (<div className={buttonClassRight} id={"4"}>
+            {showModalId === "4" && (<div className={buttonClassRight} id={"4"} style={{border: "solid 0.3vw goldenrod"}}>
                 <h4 style={{color:"goldenrod"}}>Глава 4</h4>
                 <p style={{color:"goldenrod"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque, maiores hic dignissimos ad omnis. 
                 Mollitia sunt minima ad minus quidem voluptatem soluta cum. Ad velit eligendi assumenda reiciendis aliquam?
                 </p>
-                <div className={classes.overlayImageModalHistory} style={{background:"goldenrod"}}>
-                    <img className={classes.imageModalHistory} src={Image} alt="Image4" draggable={"false"}/> 
-                </div>
+                <img className={classes.imageModalHistory} src={Caravans} alt="Caravans" draggable={"false"} style={{borderRight: "solid 1vw goldenrod", borderBottom: "solid 1vw goldenrod"}}/> 
                 <button className={classes.buttonCloseModal} id={"4"} onClick={handleCloseModal}>Закрыть</button></div>)}
             <button className={classes.buttonOpenModal5} id={"5"} onClick={() => {handleOpenModal("5")}}><p className={classes.titleChapterHistory} style={{color: "goldenrod", WebkitTextStroke: "1px black"}}>Глава 5</p></button>
-            {showModalId === "5" && (<div className={buttonClassLeft} id={"5"}>
+            {showModalId === "5" && (<div className={buttonClassLeft} id={"5"} style={{border: "solid 0.3vw goldenrod"}}>
                 <h4 style={{color:"goldenrod"}}>Глава 5</h4>
                 <p style={{color:"goldenrod"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam doloremque, maiores hic dignissimos ad omnis. 
                 Mollitia sunt minima ad minus quidem voluptatem soluta cum. Ad velit eligendi assumenda reiciendis aliquam?
                 </p>
-                <div className={classes.overlayImageModalHistory} style={{background:"goldenrod"}}>
-                    <img className={classes.imageModalHistory} src={Image} alt="Image5" draggable={"false"}/> 
-                </div>
+                <img className={classes.imageModalHistory} src={SoldiersOfRegistan} alt="SoldiersOfRegistan" draggable={"false"} style={{borderRight: "solid 1vw goldenrod", borderBottom: "solid 1vw goldenrod"}}/> 
                 <button className={classes.buttonCloseModal} id={"5"} onClick={handleCloseModal}>Закрыть</button></div>)}
         </div>
     )
