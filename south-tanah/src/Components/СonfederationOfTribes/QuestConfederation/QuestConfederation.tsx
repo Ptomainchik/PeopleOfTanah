8548b1611d749c6417215714f6a213c9eaf5c615
Ptomainchik <fantasticdenis@mail.ru>
2025-06-26 22:49:05 +0300

    AllRef1.4

--- south-tanah/src/Components/СonfederationOfTribes/QuestConfederation/QuestConfederation.tsx
+++ south-tanah/src/Components/СonfederationOfTribes/QuestConfederation/QuestConfederation.tsx
@@ -1,13 +1,13 @@
 import classes from "../../../Styles/StylesConfederationOfTribes/ConfederationOfTribesPage.module.css";
 import { BackButton } from "../../StartPage/Buttons/BackButton/BackButton";
 import { HomeButton } from "../../StartPage/Buttons/HomeButton/HomeButton";
-import { BattleInTheForest } from "./QuestLevel/BattleInTheForest/BattleInTheForest";
+import { Level1 } from "./QuestLevel/QuestLevel1";
 
 export const QuestConfederation = () => {
     return (
         <div>
             <div className={classes.topbar}><HomeButton/>Квест<BackButton/></div>
-           <BattleInTheForest/>
+           <Level1/>
         </div>
     )
 }
--- south-tanah/src/Components/СonfederationOfTribes/QuestConfederation/QuestLevel/QuestLevel1.tsx
+++ south-tanah/src/Components/СonfederationOfTribes/QuestConfederation/QuestLevel/QuestLevel1.tsx
@@ -4,9 +4,29 @@ import Tradesman from "../../../../Images/TradesmanRegistan.webp"
 
 export const Level1 = () => {
     const [showMessageQuest, setShowMessageQuest] = useState(true);
+    const [showOpeningScene, setShowOpeningScene] = useState(true);
+
+    function handleModalOpening() {
+        setShowOpeningScene(false);
+    }
 
     return (
         <div className={classes.questPage}>
+            {showOpeningScene && <div className={classes.openingScene}>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Iure molestiae facere consequuntur laudantium minima quia dicta officia! 
+                    Alias voluptates ullam pariatur ea voluptatem cumque? 
+                    Optio consequatur ipsa in tempore itaque.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae distinctio aperiam ipsa officia dolor asperiores eius numquam! Praesentium magnam rerum a, quas ab perferendis molestias similique eaque provident asperiores. Laudantium!
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. A quis, assumenda fugit facere rem architecto repellendus quasi illo, id cupiditate libero blanditiis impedit, sint doloremque dolor sapiente deserunt debitis provident.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis nulla sapiente incidunt ipsa expedita ab sunt laborum nostrum ea quibusdam accusamus eum, vitae ducimus ullam nobis. Fugit nam quaerat reiciendis!
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam delectus dolorem magnam nulla accusamus reprehenderit perspiciatis quam harum alias quasi eveniet nemo praesentium quis, odit veritatis provident dolor libero officiis.
+                </p>
+                <h3 className={classes.buttonNextScene} onClick={handleModalOpening}> 
+                    -----Продолжить-----
+                </h3>
+            </div> }
             <div className={classes.playingFieldLevel1}>
                 <img src={Tradesman} alt="Tradesman"  className={classes.characterImg1}/>
             </div>
